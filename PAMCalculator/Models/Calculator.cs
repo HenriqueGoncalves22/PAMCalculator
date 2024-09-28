8025fcfe34e69ddc436051d51b717ac6130a7d42
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2024-09-27 22:47:12 -0300

    criei o projeto

---
+++ PAMCalculator/Models/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAMCalculator.Models
+{
+    public class Calculator
+    {
+        public double PrimeiroTermo{ get; set; }
+        public double SegundoTermo{ get; set; }
+        public double Resultado{ get; set; }
+
+        public void Somar()
+        {
+            Resultado = PrimeiroTermo + SegundoTermo;
+        }
+        public void Subtrair()
+        {
+            Resultado = PrimeiroTermo - SegundoTermo;
+        }
+        public void Multiplicar()
+        {
+            Resultado = PrimeiroTermo * SegundoTermo;
+        }
+        public void Dividir()
+        {
+            Resultado = PrimeiroTermo / SegundoTermo;
+        }
+        public void Elevar()
+        {
+           
+        }
+        public void Raiz()
+        {
+        }
+    }
+}
